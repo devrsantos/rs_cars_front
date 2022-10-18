6cd64da84a2d9c863888cdde33186f91c1ac70b9
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-10-18 00:09:17 -0300

    Começando a Mexer com Inputs

--- src/Templates/Home/index.jsx
+++ src/Templates/Home/index.jsx
@@ -12,7 +12,8 @@ class Home extends Component {
     database: [],
     page: 0,
     carsPerPage: 2,
-    allCars: []
+    allCars: [],
+    searchValue: ''
   }
 
   async componentDidMount() {
@@ -42,23 +43,35 @@ class Home extends Component {
       database,
       page: nextPage
     })
+  }
 
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value })
   }
 
   render() {
-    const { database, page, carsPerPage, allCars } = this.state;
+    const { database, page, carsPerPage, allCars, searchValue } = this.state;
 
     const noMoreCars = (page + carsPerPage) >= allCars.length;
 
     return (
       <>
+        <div className='titulo'>
+          <h1>Santos Veiculos</h1>
+        </div>
+        <div className='inputBusca'>
+          <input 
+            type="text" placeholder='O que procura?' onChange={this.handleChange} value={searchValue}
+          />
+        </div>
         <div className="cars">
           {database.map(data => (
             <CarsCard key={data.id_veiculo} data={data} />
           ))}
         </div>
         <div className="container-button">
-          <Button onClick={this.loadMoreCars} disabled={noMoreCars} text="Load More Cars" />
+          <Button onClick={this.loadMoreCars} disabled={noMoreCars} text="Veja mais opções" />
         </div>
       </>
     );
